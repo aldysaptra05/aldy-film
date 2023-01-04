c595b26f4bfd9b9225ff8e3c03c62b78854ef319
aldysaptra05 <aldysaptra05@gmail.com>
2023-01-04 22:33:39 +0700

    Menambahkan semua file awal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './components/NavigationBar';
+import Intro from './components/Intro';
+import Trending from './components/Trending'
+import SuperHero from './components/Superhero';
+import "../src/style/landingPage.css"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+    {/* Intro section */}
+    <div className='myBG'>
+      <NavigationBar />
+      <Intro />
     </div>
+    {/* end of intro */}
+
+    {/* Trending section */}
+    <div className='trending'>
+      <Trending />
+    </div>
+    {/* end of trending section */}
+
+    {/* SuperHero section */}
+    <div className='superhero'>
+      <SuperHero />
+    </div>
+    {/* end of SuperHero section */}
+
+   </div>
   );
 }
 
